7481de8de02960c7abb676e28a6ece19c7a2e76a
Roman182 <60332815+Roman182@users.noreply.github.com>
2020-02-20 22:18:29 +0600

    Add files via upload

---
+++ Bicycle_Test.java
@@ -0,0 +1,15 @@
+package lab5;
+
+public class Bicycle_Test {
+
+	public static void main(String[] args) {
+		
+		
+		Bicycle B1 =new Bicycle ( 4,17);
+		B1.Show();
+		B1.Apply_Break(60);
+		B1.speed(400);
+		
+	}
+
+}
